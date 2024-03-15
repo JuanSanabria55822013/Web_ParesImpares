12460768754e97fb49c0322344c989a8e2a051fe
JuanSanabria55822013 <jssanabriam@uniboyaca.edu.co>
2024-03-15 15:30:56 -0500

    Después de mucha prueba y error se logro hacer funcionar el objetivo principal en js usando solo un for, pero con varios if, además de agregarse la funcionalidad de que mande una advertencia si introduce un número menor o un número mayor y se le termino dando los detalles finales al css, culminando con el ejercicio propuesto

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,41 @@
 function mostrarNumeros() {
-    const selector = document.getElementById('selector').value
-    const numero = document.getElementById('numero').value
-    const listaNumeros = document.getElementById('listaNumeros')
-    listaNumeros.innerHTML = ''
-}
+    const selector = document.getElementById('selector').value;
+    const numero = document.getElementById('numero').value;
+    const listaNumeros = document.getElementById('listaNumeros');
+    listaNumeros.innerHTML = '';
+
+ if(selector === 'parMay' || selector === 'imparMay'){
+    if (numero < 1 || numero > 199) {
+        alert("Por favor, introduce un número entre 1 y 199.");
+        return;
+    }
+ }
+
+    for (let i = 200; i >= 1; i--) {
+      if (selector === 'selector') {
+        listaNumeros.innerHTML += '<li>' + i + '</li>';
+      } else if (selector === 'par' && i % 2 === 0) {
+        listaNumeros.innerHTML += '<li>' + i + '</li>';
+      } else if (selector === 'impar' && i % 2 !== 0) {
+        listaNumeros.innerHTML += '<li>' + i + '</li>';
+      } else if (selector === 'parMay' && i % 2 === 0 && i > numero) {
+        listaNumeros.innerHTML += '<li>' + i + '</li>';
+      } else if (selector === 'imparMay' && i % 2 !== 0 && i > numero) {
+        listaNumeros.innerHTML += '<li>' + i + '</li>';
+      }
+    }
+  }
   
-document.getElementById('selector').addEventListener('change', function() {
-    const selector = document.getElementById('selector').value
-    const numeroInput = document.getElementById('numero')
+  document.getElementById('selector').addEventListener('change', function() {
+    const selector = document.getElementById('selector').value;
+    const numeroInput = document.getElementById('numero');
     if (selector === 'parMay' || selector === 'imparMay') {
         numeroInput.classList.remove("inputNormal")
         numeroInput.classList.add("inputMayor")
     } else {
         numeroInput.classList.remove("inputMayor")
         numeroInput.classList.add("inputNormal")
-        }
-})
+    }
+  });
+  
+  document.getElementById('verNum').addEventListener('click', mostrarNumeros);
