0ee80de84eea9e193caa654cf61c08145e8584ec
JuanSanabria55822013 <jssanabriam@uniboyaca.edu.co>
2024-03-15 15:00:37 -0500

    inicio del js nombramiento del selector, del input, la lista y una pequeña prueba del if para que aperezca el input al seleccionar una opcion especifica

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,18 @@
-let numeros = []
-for(let i = 1; i<200;i++){
-    numeros.push(i)
+function mostrarNumeros() {
+    const selector = document.getElementById('selector').value
+    const numero = document.getElementById('numero').value
+    const listaNumeros = document.getElementById('listaNumeros')
+    listaNumeros.innerHTML = ''
 }
-console.log(numeros)
+  
+document.getElementById('selector').addEventListener('change', function() {
+    const selector = document.getElementById('selector').value
+    const numeroInput = document.getElementById('numero')
+    if (selector === 'parMay' || selector === 'imparMay') {
+        numeroInput.classList.remove("inputNormal")
+        numeroInput.classList.add("inputMayor")
+    } else {
+        numeroInput.classList.remove("inputMayor")
+        numeroInput.classList.add("inputNormal")
+        }
+    })
